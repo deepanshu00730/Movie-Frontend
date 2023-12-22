3bca518c10e7a19083f148031033d095daa665b6
Deepanshu <deepanshuy00730@gmail.com>
2023-12-23 02:01:19 +0530

    Adding all files for the first time..

---
+++ src/api/axiosConfig.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export default axios.create({
+    //baseURL:'https://9028-2405-201-301b-3129-c551-6c86-e494-7bc3.ngrok-free.app/',
+    baseURL: 'http://localhost:8080/',
+    headers: {"ngrok-skip-browser-warning": "true"}
+});
